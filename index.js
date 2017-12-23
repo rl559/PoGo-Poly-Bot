f1fcdb13304748c20668115433c0f718ada19eb6
trev123 <bookwormspy@att.net>
2017-12-23 13:26:59 -0500

    grammer and stuff

--- index.js
+++ index.js
@@ -94,11 +94,11 @@ client.on("guildMemberAdd", (member) => {
 	console.log(`New User "${member.user.username}" has joined "${member.guild.name}"`);
 	member.user.send(`Welcome! ${member.user.username} to **${member.guild.name}**
   Here are a few things to start with:
-  1. __**Please**__ go ahead and read the **#rules** channel for general information as well as rules and guidelines on how to use the **PoGoPoly** discord server.
-  2. __**Please Do Not**__ Post Raids in the **#general** channel use the **#raids** channel. It helps keep the conversations organized.  
-  3. If you don't see any of the other channels please be patient contact an admin someone will give you permissions as soon as possible.
-  4. Please at least add your first name to your nickname/username by clicking on the cog icon next to your name, you will need to enter your password. To change your nickname you can use the \`.callme\` command followed by the nickname you want to use. Your nickname will only change in this server.
-  5. Use @here to notify active user in the current channel. It will help you call the attention of people that may be interested in your post, but __please do not missuse__ @here use it only when absolutely necessary.
+  1. __**Please**__ go ahead and read the **#rules** channel for general information, as well as rules and guidelines on how to use the **PoGoPoly** discord server.
+  2. __**Please Do Not**__ post raids in the **#general** channel, use the **#raids** channel. It helps keep the conversations organized.  
+  3. If you don't see any of the other channels please be patient. Contact an admin, and someone will give you permissions as soon as possible.
+  4. To change your nickname, you can use the \`.callme\` command followed by the nickname you want to use. Your nickname will only change in this server.  Changing it to your PoGo account name will help others identify you.
+  5. Use @here to notify active users in the current channel. It will help you call the attention of people that may be interested in your post, but __please do not misuse__ @here. Use it only when absolutely necessary.
   6. Use the voice channels in times of inclement weather.
   7. Under no circumstances should you cause drama or fight with other members. If you need to talk, create a private group chat and talk it out there.
   Happy Hunting, hope you catch 'em all.`);
@@ -135,7 +135,7 @@ function everyHour(){
 			if ( newUsers[index] && newUsers[index].size > 0 && timesAnHour == 0) {//newUsers.size > 10
 				//console.log('its pass 9');
 				const userlist = newUsers[index].map(u => u.toString()).join(" ");
-				item.channels.get(index).send("Welcome our new CS Raidrz!\n" + userlist);
+				item.channels.get(index).send("Welcome our new members!\n" + userlist);
 				newUsers[index].clear();
 			}
 
@@ -184,7 +184,7 @@ client.on("message", (message) => {
 		channelName = message.channel.name,
 		currentTime = Moment().tz('America/New_York');
 	
-	if( message.system && message.type === 'PINS_ADD' && cityPattern.test(channelName) && message.author.username === 'CS Raidr Bot' ){
+	if( message.system && message.type === 'PINS_ADD' && cityPattern.test(channelName) && message.author.username === 'PoGoPolyBot' ){
 		//console.log( message );
 		message.delete();
 	}
@@ -303,7 +303,7 @@ client.on("message", (message) => {
 		let pattern = prefix + "help",
 			helpType = message.content.substr(message.content.indexOf(pattern) + pattern.length).trim().toLocaleLowerCase(),
 			channelVal = '\n\n__**Channels**__\n\n**#general** this is for general talk. No raid coordination allowed. \n**#announcements** this is were we let you know the news about the game and our server. \n**#commands** this is were you have the freedom to use all commands in our server. \n**#city_maps** this is where you can find information about city bounds and some useful addresses \n**#nest** this is where you\'ll find people posting about nests. \n**#rare_pokemon_sightings** this is where you\'ll find people posting about rare sightings \n**#gallery** this is where people showcase images taken in the field. \nAfter this you\'ll find all the city channels where Raids are coordinated. \n',
-			commandVal = '\n\n__**Commands**__\n\n**.address** this command retrieves the top result from GMaps. usage: `.address mullins park coral springs`\n**.callme** this command changes your nickname on our server. usage: `.callme CS Raidr BOT there\'s no one better there\'s no one greater`\n**.help** this is commandseption.\n**.iam** this command adds roles to your profile, to know what roles you can add use `.help roles`, usage: `.iam mystic` or `.iam mystic,coral springs`.\n**.iamnot** this command removes roles from your profile, to know what roles you can remove use `.help roles`, usage: `.iamnot mystic` or `.iamnot mystic,coral springs`.\n**.invite** this command creates an invite you can send your friends. \n**.pokedex** this command retrieves a pokemon entry from the pokedex. usage: `.pokedex bulbasaur`',
+			commandVal = '\n\n__**Commands**__\n\n**.address** this command retrieves the top result from GMaps. usage: `.address mullins park coral springs`\n**.callme** this command changes your nickname on our server. usage: `.callme PoGoPolyBot there\'s no one better there\'s no one greater`\n**.help** this is commandseption.\n**.iam** this command adds roles to your profile, to know what roles you can add use `.help roles`, usage: `.iam mystic` or `.iam mystic,coral springs`.\n**.iamnot** this command removes roles from your profile, to know what roles you can remove use `.help roles`, usage: `.iamnot mystic` or `.iamnot mystic,coral springs`.\n**.invite** this command creates an invite you can send your friends. \n**.pokedex** this command retrieves a pokemon entry from the pokedex. usage: `.pokedex bulbasaur`',
 			rolesVal = '\n\n__**Roles**__\n\n**Team Roles**\nthis roles change the color of your nickname to your team\'s color.\n\nMystic Raidrz\nValor Raidrz\nInstinct Raidrz\n\n**TIP:** you can enter just the team name the bot will recognize the appripiate role.\n\n**City Roles**\nthis roles send you a notification when the bot recognizes a raid image posted in a city you want to raid in.\n\nboca\ncoral_springs\ncoconut_creek\ndavie\ndeerfield\nft_lauderdale\nhollywood\nmargate\nnorth_lauderdale\nparkland\npompano\nplantation\nsunrise\ntamarac\n\n**TIP:** you can enter the city roles without the underscore and in lower or uppercase just keep in mind the name needs to be exact so `ft lauderdale` will work `fort lauderdale` won\'t,',
 			raidsVal = '\n\n__**Raids**__\n\nBased on our current field research Raids start at 6AM and end at 8PM, you may find active raids after 8PM, but no new ones will popup after 8PM',
 			helpContent = '',
