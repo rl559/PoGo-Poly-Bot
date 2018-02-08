3bb626beef3114821b1a91d2b7b303938f4d30c5
trev123 <bookwormspy@att.net>
2018-02-08 18:02:00 -0500

    fixing b

--- classes/Btext.js
+++ classes/Btext.js
@@ -2,7 +2,7 @@ module.exports = class GymNotice
 {
   process(message, prefix)
   {
-    var msgText = message.content.substring(".b ");
+    var msgText = message.content.substring(str.indexOf(".b "));
     console.log(msgText);
   }
 }
