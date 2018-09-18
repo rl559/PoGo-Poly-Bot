4a07f9cfff5c5b2563d46e0f64a7edd403cbb0fa
Justin Crampton <38043472+cowcow923@users.noreply.github.com>
2018-09-18 15:35:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -541,7 +541,7 @@ client.on("message", (message) => {
 		//The following is an experiment to see if I can actually link the channel .egg creates
 		var callRole = message.channel.guild.roles.find('name', 'T1');
 		var newChannel = message.channel.guild.channels.find("name", "level-1-raid-hatchtime-1100");
-                message.channel.send("A " + callRole + " raid has been reported by " + message.author + "!");
+                message.channel.send("A " + callRole + " raid has been reported by " + message.author + "! Go to " + newChannel);
 		
 		//Test 2 (maybe)
 		/*
